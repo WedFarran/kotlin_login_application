6445f334070c6a6fa12646f412c265205b840751
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2024-02-27 02:10:08 +0300

    sign up UI is done

--- app/src/main/java/com/example/kotlinsplashscreenapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinsplashscreenapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.kotlinsplashscreenapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.kotlinsplashscreenapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            var intent = Intent(this, login_screen::class.java)
+            startActivity(intent)
+            finish()
+        },3000
+        )
+
     }
 }
--- app/src/main/java/com/example/kotlinsplashscreenapp/SignUpScreen.kt
+++ app/src/main/java/com/example/kotlinsplashscreenapp/SignUpScreen.kt
@@ -4,21 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kotlinsplashscreenapp.databinding.ActivityLoginScreenBinding
-import com.example.kotlinsplashscreenapp.databinding.ActivityMainBinding
+import com.example.kotlinsplashscreenapp.databinding.ActivitySignUpScreenBinding
 
-class login_screen : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginScreenBinding
+class SignUpScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        binding = ActivitySignUpScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-        binding.tvMoveToSignUp.setOnClickListener{
-            var intent = Intent(this, SignUpScreen::class.java)
+        binding.textView6.setOnClickListener{
+            var intent = Intent(this, login_screen::class.java)
             startActivity(intent)
             finish()
         }
-        binding.btLogin.setOnClickListener{
+        binding.btSignup.setOnClickListener{
             var intent = Intent(this, MainScreen::class.java)
             startActivity(intent)
             finish()
