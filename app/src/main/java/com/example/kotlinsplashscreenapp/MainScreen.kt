62025d19b9ee70b944971ac5d0385b083810ae65
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2024-03-02 11:05:42 +0300

    drawer layout added

--- app/src/main/java/com/example/kotlinsplashscreenapp/MainScreen.kt
+++ app/src/main/java/com/example/kotlinsplashscreenapp/MainScreen.kt
@@ -2,10 +2,40 @@ package com.example.kotlinsplashscreenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.kotlinsplashscreenapp.databinding.ActivityMainScreenBinding
 
 class MainScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityMainScreenBinding
+    private lateinit var toggle :ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_screen)
+        binding = ActivityMainScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout,R.string.open,R.string.close)
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.profile -> Toast.makeText(this, "profile",Toast.LENGTH_SHORT).show()
+                R.id.trending-> Toast.makeText(this, "trending",Toast.LENGTH_SHORT).show()
+                R.id.sign_out -> Toast.makeText(this, "sign_out",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
