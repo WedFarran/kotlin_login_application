037baee82732f4b572e15b0a269bf7a1800fe442
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2024-03-05 00:48:55 +0300

    fragments added

---
+++ app/src/main/java/com/example/kotlinsplashscreenapp/favoriteFragment.kt
@@ -0,0 +1,6 @@
+package com.example.kotlinsplashscreenapp
+
+import androidx.fragment.app.Fragment
+
+class favoriteFragment : Fragment(R.layout.fragment_favorite) {
+}
---
+++ app/src/main/java/com/example/kotlinsplashscreenapp/musicFragment.kt
@@ -0,0 +1,6 @@
+package com.example.kotlinsplashscreenapp
+
+import androidx.fragment.app.Fragment
+
+class musicFragment : Fragment(R.layout.fragment_music) {
+}
